64d90d4415d5f2322d38589b2f3e07c39c55149e
torlentzhang <1677077188@qq.com>
2025-07-11 10:38:01 +0800

    引入vant并使用

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
   <div id="app">
+    <van-button type="primary">主要按钮</van-button>
+    <van-cell is-link @click="showPopup">展示弹出层</van-cell>
+    <van-popup v-model="show">内容</van-popup>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: false
+    }
+  },
+
+  methods: {
+    showPopup () {
+      this.show = true
+    }
+  }
+}
+</script>
 
 <style lang="less">
 
