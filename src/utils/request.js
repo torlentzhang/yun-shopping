954f5f2b46f5398ba7539e230296ba521c74f8e1
torlentzhang <1677077188@qq.com>
2025-07-11 17:28:51 +0800

    axios封装并测试

---
+++ src/utils/request.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+// 创建axios实力，对将来创建出来的实例自定义配置
+// 这样写不会污染原始axios实力
+const instance = axios.create({
+  baseURL: 'http://smart-shop.itheima.net/index.php?s=/api',
+  timeout: 5000,
+  headers: { platform: 'App' }
+})
+
+// 自定义配置
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response.data
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error)
+})
+
+// 导出
+export default instance
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,99 +3,104 @@
     <!-- 头部 -->
     <van-nav-bar title="用户登录" left-arrow @click-left="$router.go(-1)" />
     <!-- 主体 -->
-    <div class="main">
-      <h2>手机号登录</h2>
-      <p>未注册的手机号登录后将自动注册</p>
+    <div class="container">
+      <div class="title">
+        <h3>手机号登录</h3>
+        <p>未注册的手机号登录后将自动注册</p>
+      </div>
+
       <div class="form">
         <div class="form-item">
-          <input type="text" placeholder="请输入手机号码">
+          <input class="inp" maxlength="11" placeholder="请输入手机号码" type="text">
         </div>
         <div class="form-item">
-          <input type="text" placeholder="请输入图形验证码">
+          <input class="inp" maxlength="5" placeholder="请输入图形验证码" type="text">
           <img src="@/assets/code.png" alt="">
         </div>
         <div class="form-item">
-          <input type="text" placeholder="请输入短信验证码">
+          <input class="inp" placeholder="请输入短信验证码" type="text">
           <button>获取验证码</button>
         </div>
       </div>
+
+      <div class="login-btn">登录</div>
     </div>
-    <div class="login-btn">登录</div>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request'
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  async created () {
+    const res = await request.get('/captcha/image')
+    console.log(res)
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.main {
-  margin-left: 25px;
-  margin-top: 50px;
+.container {
+  padding: 49px 29px;
 
-  h2 {
-    font-size: 30px;
-    font-weight: 400;
-  }
+  .title {
+    margin-bottom: 20px;
 
-  p {
-    margin-top: 10px;
-    font-size: 14px;
-    color: grey
-  }
+    h3 {
+      font-size: 26px;
+      font-weight: normal;
+    }
 
-  .form {
-    margin-top: 20px;
-
-    .form-item {
-      border-bottom: 1px solid #f3f1f2;
-      padding: 8px 0;
-      margin-bottom: 14px;
-      display: flex;
-      align-items: center;
-
-      input {
-        display: block;
-        border: none;
-        outline: none;
-        height: 32px;
-        font-size: 14px;
-      }
-
-      img {
-        flex: 1;
-        margin-left: 80px;
-        height: 31px;
-      }
-
-      button {
-        flex: 1;
-        margin-left: 80px;
-        height: 31px;
-        border: none;
-        font-size: 13px;
-        color: orange;
-        background-color: transparent;
-      }
+    p {
+      line-height: 40px;
+      font-size: 14px;
+      color: #b8b8b8;
     }
   }
-}
 
-.login-btn {
-  width: 100%;
-  height: 42px;
-  margin-top: 40px;
-  box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.2);
-  letter-spacing: 2px;
-  border-radius: 39px;
-  background-color: lightsalmon;
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 25px;
-  margin-left: 0;
+  .form-item {
+    border-bottom: 1px solid #f3f1f2;
+    padding: 8px;
+    margin-bottom: 14px;
+    display: flex;
+    align-items: center;
+
+    .inp {
+      display: block;
+      border: none;
+      outline: none;
+      height: 32px;
+      font-size: 14px;
+      flex: 1;
+    }
+
+    img {
+      width: 94px;
+      height: 31px;
+    }
+
+    button {
+      height: 31px;
+      border: none;
+      font-size: 13px;
+      color: #cea26a;
+      background-color: transparent;
+      padding-right: 9px;
+    }
+  }
+
+  .login-btn {
+    width: 100%;
+    height: 42px;
+    margin-top: 39px;
+    background: linear-gradient(90deg, #ecb53c, #ff9211);
+    color: #fff;
+    border-radius: 39px;
+    box-shadow: 0 10px 20px 0 rgba(0, 0, 0, .1);
+    letter-spacing: 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
